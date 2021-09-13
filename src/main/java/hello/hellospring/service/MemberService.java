a329fcf43e3d7ee7d10328becb76e55e19679f19
Jisu Choi <gjkl55@naver.com>
2021-09-13 20:46:19 +0900

    [추가] 회원 Service 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     *
+     * 회원 가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원 X
+        //과거에는 if로 null 처리를 하였지만 지금은 Optinal로 한 번 감싸서 확인
+        //코드가 안 이쁘면 이렇게 할 수 있음
+        validateDulicateMember(member); //중복회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDulicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     *
+     * id로 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
