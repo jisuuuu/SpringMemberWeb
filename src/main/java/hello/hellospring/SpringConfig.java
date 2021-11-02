1a4cfb6b3290d9c7fbbd9d2ec3b4a30cc851d194
Jisu Choi <gjkl55@naver.com>
2021-11-02 21:41:41 +0900

    [추가] 자바코드로 직접 스프링 빈 등록하기 위한 config class

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,25 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    /*
+    상황에 따라 구현 클래스를 변경해야할 때 설정을 통해 스프링 빈으로 등록함
+    Component 주입은 불가
+     */
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
